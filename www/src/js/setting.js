c6b72f652a94f9b13e99fd49a4a9fd39fd1b947c
Raphael <raphael.scharff@gmail.com>
2025-09-21 22:51:53 +0200

    User defined compressor

--- www/src/js/setting.js
+++ www/src/js/setting.js
@@ -11,8 +11,8 @@ function settingLoad() {
     document.getElementById("setting-user-uuid").innerText = global.user.id;
     document.getElementById("setting-user-name").value = global.user.displayName;
     document.getElementById("setting-user-theme").value = localStorage.getItem("Theme");
+    settingCompressorShow();
     selectSettingItem("overview");
-    settingCompressorLoad();
 }
 
 function selectSettingItem(name) {
@@ -83,28 +83,102 @@ function settingPassword() {
     });
 }
 
-function settingCompressorLoad(){
-    document.getElementById('compressor-enabled').checked = voice.compressorSetting.enabled ? "checked" : "";
+function settingCompressorDirectShow(param, element) {
+    switch (param) {
+        case 'attack':
+            document.getElementById('compressor-attack-label').innerText = `Attack : ${element.value * 1000}ms`;
+            break;
+        case 'ratio':
+            document.getElementById('compressor-ratio-label').innerText = `Ratio : ${element.value}`;
+            break;
+        case 'reduction':
+            document.getElementById('compressor-reduction-label').innerText = `Reduction : ${element.value}dB`;
+            break;
+        case 'release':
+            document.getElementById('compressor-release-label').innerText = `Release : ${element.value * 1000}ms`;
+            break;
+        case 'threshold':
+            document.getElementById('compressor-threshold-label').innerText = `Threshold : ${element.value}dB`;
+            break;
+    }
+}
+
+function settingCompressorShow() {
+    const buttonEnabled = document.getElementById('compressor-enabled')
+    if (voice.compressorSetting.enabled) {
+        buttonEnabled.innerText = "Enabled";
+        buttonEnabled.classList.remove("disabled");
+        buttonEnabled.classList.add("enabled");
+    }
+    else {
+        buttonEnabled.innerText = "Disabled";
+        buttonEnabled.classList.add("disabled");
+        buttonEnabled.classList.remove("enabled");
+    }
 
     document.getElementById('compressor-attack').value = voice.compressorSetting.attack;
     document.getElementById('compressor-attack').title = voice.compressorSetting.attack * 1000 + "ms";
-
-    document.getElementById('compressor-knee').value = voice.compressorSetting.knee;
-    document.getElementById('compressor-knee').title = voice.compressorSetting.knee;
+    document.getElementById('compressor-attack-label').innerText = `Attack : ${voice.compressorSetting.attack * 1000}ms`;
 
     document.getElementById('compressor-ratio').value = voice.compressorSetting.ratio;
     document.getElementById('compressor-ratio').title = voice.compressorSetting.ratio;
+    document.getElementById('compressor-ratio-label').innerText = `Ratio : ${voice.compressorSetting.ratio}`;
 
     document.getElementById('compressor-reduction').value = voice.compressorSetting.reduction;
     document.getElementById('compressor-reduction').title = voice.compressorSetting.reduction + "dB";
+    document.getElementById('compressor-reduction-label').innerText = `Reduction : ${voice.compressorSetting.reduction}dB`;
 
     document.getElementById('compressor-release').value = voice.compressorSetting.release;
     document.getElementById('compressor-release').title = voice.compressorSetting.release * 1000 + "ms";
+    document.getElementById('compressor-release-label').innerText = `Release : ${voice.compressorSetting.release * 1000}ms`;
 
     document.getElementById('compressor-threshold').value = voice.compressorSetting.threshold;
     document.getElementById('compressor-threshold').title = voice.compressorSetting.threshold + "dB";
+    document.getElementById('compressor-threshold-label').innerText = `Threshold : ${voice.compressorSetting.threshold}dB`;
 }
 
-function settingCompressorUpdate(type, data){
+function settingCompressorEnabled() {
+    voice.compressorSetting.enabled = !voice.compressorSetting.enabled;
+    saveUserSetting();
+    settingCompressorShow();
+}
 
+function settingCompressorUpdate(param, data) {
+    switch (param) {
+        case 'enabled':
+            voice.compressorSetting.enabled = data.checked === "checked";
+            break;
+        case 'attack':
+            voice.compressorSetting.attack = parseFloat(data.value);
+            break;
+        case 'ratio':
+            voice.compressorSetting.ratio = parseInt(data.value);
+            break;
+        case 'reduction':
+            voice.compressorSetting.reduction = parseFloat(data.value);
+            break;
+        case 'release':
+            voice.compressorSetting.release = parseFloat(data.value);
+            break;
+        case 'threshold':
+            voice.compressorSetting.threshold = parseInt(data.value);
+            break;
+    }
+
+    saveUserSetting();
+    settingCompressorShow();
+}
+
+function settingCompressorDefault() {
+    voice.compressorSetting = {
+        enabled: true,
+        attack: 0,
+        knee: 40,
+        ratio: 12,
+        reduction: 0,
+        release: 0.25,
+        threshold: -50,
+    }
+    saveUserSetting();
+    settingCompressorShow();
 }
