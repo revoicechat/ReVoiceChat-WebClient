3f3dcee7e41e128214821754f84e950a1f68ebde
Charles Lgn <charles.ligony@gmail.com>
2025-12-13 13:07:52 +0100

    fix(mobile): toggle sidebar error

--- www/src/js/mobile.js
+++ www/src/js/mobile.js
@@ -1,25 +1,22 @@
 const toggleSidebar = document.getElementById('toggleSidebar');
 const toggleUsers = document.getElementById('toggleUsers');
-const sidebar = document.querySelector('.sidebar');
-const sidebarRight = document.querySelector('.sidebar-right');
+const sidebar = document.querySelector('.sidebar.left');
+const sidebarRight = document.getElementById('sidebar-users');
 const overlay = document.getElementById('overlay');
 
 function closePanels() {
-  console.log("closePanels")
   sidebar.classList.remove('show');
   sidebarRight.classList.remove('show');
   overlay.classList.remove('show');
 }
 
 toggleSidebar.addEventListener('click', () => {
-  console.log("toggleSidebar")
   sidebar.classList.toggle('show');
   sidebarRight.classList.remove('show');
   overlay.classList.toggle('show');
 });
 
 toggleUsers.addEventListener('click', () => {
-  console.log("toggleUsers")
   sidebarRight.classList.toggle('show');
   sidebar.classList.remove('show');
   overlay.classList.toggle('show');
